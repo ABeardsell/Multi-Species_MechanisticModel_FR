ef17c03e3bb9ca57884ba620a563e4e3d8d0ad8a
Andreanne <abeardsell@hotmail.com>
2022-10-17 11:22:52 -0400

    Code Nesting Success Geese + Parameter Values and MSFR equations

---
+++ Code/MSFR_equations.R
@@ -0,0 +1,59 @@
+# prey 1 - Lemming
+# prey 2 - Goose nest
+# prey 3 - Sandpiper nest
+# -------------------------------------------------------
+# Compute acquisition rate of sandpiper
+# -------------------------------------------------------
+MSFR_sandpiper <- function(N1,N2,N3) {
+  phi = phi_L_concav(N1) #Phi value is a function of lemming density
+  s = s_L_concav(N1)#s value is a function of lemming density
+
+  alpha_1=s*(2*d1)*f21_f31*f41 #Capture efficiency of a lemming (km2/day)
+  alpha_3=s*(2*d3)*f23 #Capture efficiency of a sandpiper nest (km2/day)
+  alpha_2a_complete = s*f32_a*(2*d2_a)*f42_a*p_c_a #Capture rate of a nest by a predator - COMPLETE
+  alpha_2a_partial = (s*f32_a*(2*d2_a)*f42_a*(1-p_c_a))/3.7 #Capture rate of a nest by a predator - PARTIAL
+  alpha_2a = alpha_2a_complete + alpha_2a_partial # Predation totale
+  alpha_2ua_complete = s*f22_ua*(2*d2_ua)*p_c_ua*f42_ua #Capture rate of a nest by a predator - COMPLETE
+  alpha_2ua_partial = (s*f22_ua*(2*d2_ua)*f42_ua*(1-p_c_ua))/3.7 #Capture rate of a nest by a predator - PARTIAL
+  alpha_2ua = alpha_2ua_complete + alpha_2ua_partial # Predation totale
+
+  h_1=  (Tp1/f41) + ((Te1 * e1) + (To1 * o1) + (Tde1*de1)) #Handling time of a lemming
+  h_2ua= Tp2/(f42_ua * p_c_ua) + Tm2 #Handling time of a nest unattended
+  h_2a= Tp2/(f42_a * p_c_a) + Tm2 #Handling time of a nest attended
+  h_3= Tm3 #consumption time of a sandpiper nest
+
+  AR3 <- (alpha_3 * N3 * phi)/(1+(alpha_1 * h_1 * N1) + (alpha_3 * h_3* N3)
+      + (alpha_2ua * h_2ua* ((1-w)*N2)) + (alpha_2a * h_2a* (w*N2)))
+  return(AR3)
+}
+
+# -------------------------------------------------------
+# Compute acquisition rate of goose nests
+# -------------------------------------------------------
+
+MSFR_goose <- function(N1,N2,N3) {
+  phi = phi_L_concav(N1) #Phi value is a function of lemming density
+  s = s_L_concav(N1) #s value is a function of lemming density
+
+  alpha_1=s*(2*d1)*f21_f31*f41 #Capture efficiency of a lemming (km2/day)
+  alpha_3=s*(2*d3)*f23 #Capture efficiency of a passerine nest (km2/day)
+  alpha_2a_complete = s*f32_a*(2*d2_a)*f42_a*p_c_a #Capture rate of a nest by a predator - COMPLETE
+  alpha_2a_partial = (s*f32_a*(2*d2_a)*f42_a*(1-p_c_a))/3.7 #Capture rate of a nest by a predator - PARTIAL
+  alpha_2a = alpha_2a_complete + alpha_2a_partial # Predation totale
+  alpha_2ua_complete = s*f22_ua*(2*d2_ua)*p_c_ua*f42_ua #Capture rate of a nest by a predator - COMPLETE
+  alpha_2ua_partial = (s*f22_ua*(2*d2_ua)*f42_ua*(1-p_c_ua))/3.7 #Capture rate of a nest by a predator - PARTIAL
+  alpha_2ua = alpha_2ua_complete + alpha_2ua_partial # Predation totale
+
+  h_1=  (Tp1/f41) + ((Te1 * e1) + (To1 * o1) + (Tde1*de1)) #Handling time of a lemming
+  h_2ua= Tp2/(f42_ua * p_c_ua) + Tm2 #Handling time of a nest unattended
+  h_2a= Tp2/(f42_a * p_c_a) + Tm2 #Handling time of a nest attended
+  h_3= Tm3 #consumption time of a sandpiper nest
+
+  AR2ga <- (alpha_2a * (w*N2) * phi)/(1+(alpha_1 * h_1 * N1) + (alpha_3 * h_3* N3)
+        + (alpha_2ua * h_2ua* ((1-w)*N2)) + (alpha_2a * h_2a* (w*N2)))
+  AR2gua <- (alpha_2ua * ((1-w)*N2) * phi)/(1+(alpha_1 * h_1 * N1) + (alpha_3 * h_3* N3)
+        + (alpha_2ua * h_2ua* ((1-w)*N2)) + (alpha_2a * h_2a* (w*N2)))
+  AR2=AR2ga+AR2gua
+  return(AR2)
+  #list("ARl"=ARl,"ARp"=ARp,"ARs"=ARs,"ARg"=ARg,"ARga"=ARga,"ARgua"=ARgua,"L"=x,"P"=y,"S"=z,"G"=a)
+}
---
+++ Code/Parameters_values.R
@@ -0,0 +1,44 @@
+#--------------------------------------------------------------------
+# MSFR parameters
+#--------------------------------------------------------------------
+# general parameters
+s=93 # daily distance the predator traveled  - km/day
+phi=0.5 #average proportion of time the predator spent active
+
+# Lemmings (prey 1)
+d1=0.0075 #maximum reaction distance - km
+f21_f31=0.15 # Average detection and attack probabilities of a lemming
+f41=0.51 # Average success probability of an attack
+Tp1 = 0.024/24 #Pursue time - day/nest
+Te1= 0.00038 #consumption time - day/nest
+e1= 0.48 # consumption probability
+To1= 0.000486 # hoarding time - day/nest
+o1= 0.32 # hoarding probability
+Tde1= 0.00388# delivery time- day/nest
+de1= 0.20 #delivery probability
+
+# Geese (prey 2)
+w= 1-0.021 #0.021 # nest attendance prob.
+d2_a=0.033 #Maximum reaction distance - km
+d2_ua=0.11 #Maximum reaction distance - km
+f22_ua=0.366 # detection probability
+f42_a=0.098 # success probability
+f42_ua=0.934 # success probability
+f32_a=0.05#0.11 # attack probability (nest attended)
+p_c_ua=0.69 #complete predation probability
+p_c_a=0.47 #complete predation probability
+Tp2=0.02/24 #Pursue time - day/nest
+Tm2 = 0.14/24 #Manipulation time - day/nest
+
+# Sandpiper (prey 3)
+d3=0.085 #Maximum reaction distance - km
+f23=0.029 #detection probability
+Tm3=0.069/24 #consumption time of a nest -day/nest
+
+# Density-dependent functions of s and phi to lemming density
+s_L_concav <- function(x) {
+64 + (93-64)* (exp(-x/150))
+}
+phi_L_concav <- function(x) {
+0.40 + (0.53-0.40)* (exp(-x/180))
+}
